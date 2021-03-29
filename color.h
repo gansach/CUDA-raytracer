c612632811499696c4268d265c21f151261d4dd0
gansach <sachdevagandharv@gmail.com>
2021-03-29 19:50:14 +0530

    Fixed inline operator overloading linking error

--- color.h
+++ color.h
@@ -1,6 +1,8 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "vec3.h"
+
 #include <iostream>
 
 void write_color(std::ostream &out, color pixel_color)
--- vec3.h
+++ vec3.h
@@ -57,8 +57,6 @@ public:
 using point3 = vec3;
 using color = vec3;
 
-#endif
-
 //vec3 Utility Functions
 
 inline std::ostream &operator<<(std::ostream &out, const vec3 &v)
@@ -112,3 +110,5 @@ inline vec3 unit_vector(vec3 v)
 {
     return v / v.length();
 }
+
+#endif

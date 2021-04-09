11ff3ce7302c71b6c80652a4907750090d339734
gansach <sachdevagandharv@gmail.com>
2021-04-09 22:25:09 +0530

    Implemented antialiasing and seperated camera.h

--- rtweekend.h
+++ rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Usings
 
@@ -17,6 +18,26 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
+inline double clamp(double x, double min, double max)
+{
+    if (x < min)
+        return min;
+    if (x > max)
+        return max;
+    return x;
+}
+
+inline double random_double()
+{
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
 
 inline double degrees_to_radians(double degrees)
 {
